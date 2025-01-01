d8d7b5c961028f0975c2ef373690b1e5cd62cb02
abu bakar <abubakar850772@gmail.com>
2025-01-01 23:56:15 +0600

    add todo slice with initial state and addTask reducer

---
+++ Shadcn/src/redux/features/todo/todoSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+type Todo = {
+    tasks: [
+        {
+            id: string;
+			date: Date;
+			title: string;
+            description: string;
+			status: "DONE" | "PENDING" | "CANCELED";
+		}
+	];
+};
+
+const initialState: Todo = {
+    tasks: [
+        {
+            id: "233",
+			date: new Date(),
+			title: "Initial Shadcn project",
+            description: "setup project using vite",
+			status: "DONE",
+		}
+	],
+};
+
+const todoSlice = createSlice({
+	name: "todo",
+	initialState,
+	reducers: {
+		// add a new task
+
+		addTask() {
+
+        },
+	},
+});
+
+
+export const {addTask} = todoSlice.actions;
+
+const todoReducer = todoSlice.reducer;
+
+export default todoReducer;
