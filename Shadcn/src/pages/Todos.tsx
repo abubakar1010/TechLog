09365b8a033b4425efc4d475c304e156dc4bdc49
abu bakar <abubakar850772@gmail.com>
2025-01-02 11:33:54 +0600

    feat: integrate AddTaskModal into Todos page for task creation

--- Shadcn/src/pages/Todos.tsx
+++ Shadcn/src/pages/Todos.tsx
@@ -1,19 +1,22 @@
+import { AddTaskModal } from "@/components/modules/todo/AddTaskModal";
 import { TodoCard } from "@/components/modules/todo/todo-card";
 import { useAppSelector } from "@/redux/app/hooks";
 import { selectTasks } from "@/redux/features/todo/todoSlice";
 
 const Todos = () => {
-    const tasks = useAppSelector(selectTasks)
-    return (
-        <div>
-            <p>This is task page</p>
-            <div className=" space-y-5">
-                {
-                    tasks.map( task => <TodoCard key={task.id} task={task} />)
-                }
-            </div>
-        </div>
-    );
+	const tasks = useAppSelector(selectTasks);
+	return (
+		<div>
+			<div className="flex justify-end items-center">
+				<AddTaskModal />
+			</div>
+			<div className=" space-y-5 my-8">
+				{tasks.map((task) => (
+					<TodoCard key={task.id} task={task} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Todos;
