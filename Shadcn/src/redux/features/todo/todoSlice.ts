1276985abb1d8408ad331e3c98c9d429c559f4de
abu bakar <abubakar850772@gmail.com>
2025-01-03 19:59:07 +0600

    feat: refactor todoSlice to improve task management and enhance isCompleteToggle functionality

--- Shadcn/src/redux/features/todo/todoSlice.ts
+++ Shadcn/src/redux/features/todo/todoSlice.ts
@@ -3,44 +3,46 @@ import { TDraftTask, TTask } from "@/types";
 import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit";
 
 type Todo = {
-    tasks: TTask[]
-	;
+	tasks: TTask[];
 };
 
 const initialState: Todo = {
-    tasks: [
-        {
-            id: nanoid(),
+	tasks: [
+		{
+			id: nanoid(),
 			deadline: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
 			title: "Complete University Management Project",
-            description: "Define project requirement, analyze requirement, choose technology according to requirement.Focus on functional requirement",
+			description:
+				"Define project requirement, analyze requirement, choose technology according to requirement.Focus on functional requirement",
 			isComplete: false,
-			priority: "HIGH"
+			priority: "HIGH",
 		},
-        {
-            id: nanoid(),
+		{
+			id: nanoid(),
 			deadline: new Date(Date.now() + 8 * 24 * 60 * 60 * 1000),
 			title: "Start the project",
-            description: "first took a pattern for maintain your project. Design database and more",
+			description:
+				"first took a pattern for maintain your project. Design database and more",
 			isComplete: false,
-			priority: "LOW"
+			priority: "LOW",
 		},
-        {
-            id: nanoid(),
+		{
+			id: nanoid(),
 			deadline: new Date(Date.now() + 5 * 24 * 60 * 60 * 1000),
 			title: "Take look on non functional requirement",
-            description: "At the beginning you should non functional attribute like performance, security, usability, reliability, scalability, maintainability, portability",
+			description:
+				"At the beginning you should non functional attribute like performance, security, usability, reliability, scalability, maintainability, portability",
 			isComplete: false,
-			priority: "MEDIUM"
-		}
+			priority: "MEDIUM",
+		},
 	],
 };
 
 const createTask = (actionData: TDraftTask): TTask => ({
 	...actionData,
 	id: nanoid(),
-	isComplete: true
-})
+	isComplete: true,
+});
 
 const todoSlice = createSlice({
 	name: "todo",
@@ -49,23 +51,23 @@ const todoSlice = createSlice({
 		// add a new task
 
 		addTask(state, action: PayloadAction<TDraftTask>) {
+			const taskData: TTask = createTask(action.payload);
 
-			const taskData: TTask = createTask(action.payload)
-			
-			state.tasks.push(taskData)
-        },
-		isCompleteToggle(state, action: PayloadAction<string>){
-			return state.tasks.forEach( item => item.id === action.payload? item.isComplete = !item.isComplete : item
-			)
-		}
+			state.tasks.push(taskData);
+		},
+		isCompleteToggle(state, action: PayloadAction<string>) {
+			return state.tasks.forEach((item) =>
+				item.id === action.payload
+					? (item.isComplete = !item.isComplete)
+					: item
+			);
+		},
 	},
 });
 
+export const { addTask, isCompleteToggle } = todoSlice.actions;
 
-export const {addTask, isCompleteToggle} = todoSlice.actions;
-
-
-export const selectTasks = (state:RootState) => state.todos.tasks
+export const selectTasks = (state: RootState) => state.todos.tasks;
 
 const todoReducer = todoSlice.reducer;
 
