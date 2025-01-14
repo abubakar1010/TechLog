d730a263d04cc557cb3b5ddecd24df29e59e1bbb
abu bakar <abubakar850772@gmail.com>
2025-01-14 16:24:57 +0600

    feat: add AdminDashboard and user management pages for admin functionality

---
+++ ums_client/src/pages/admin/userManagement/CreateAdmin.tsx
@@ -0,0 +1,7 @@
+export const CreateAdmin = () => {
+	return (
+		<div>
+			<h1>This is the CreateAdmin page</h1>
+		</div>
+	);
+};
---
+++ ums_client/src/pages/admin/userManagement/CreateFaculty.tsx
@@ -0,0 +1,7 @@
+export const CreateFaculty = () => {
+	return (
+		<div>
+			<h1>This is the CreateFaculty page</h1>
+		</div>
+	);
+};
