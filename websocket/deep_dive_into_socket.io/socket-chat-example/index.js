979fb31d401b93e3a0da34fca9a1a81b0687cc58
abu bakar <abubakar850772@gmail.com>
2025-01-29 00:05:11 +0600

    feat: emitting events

--- websocket/deep_dive_into_socket.io/socket-chat-example/index.js
+++ websocket/deep_dive_into_socket.io/socket-chat-example/index.js
@@ -24,6 +24,9 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
         console.log("a user is disconnected")
     })
+    socket.on("chat message", (msg) => {
+        console.log({message: msg})
+    })
 })
 
 httpServer.listen(port, () => {
