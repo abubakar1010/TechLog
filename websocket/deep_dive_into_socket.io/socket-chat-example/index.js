5c3792fd446c40971fb51504a2075a29c81a2da0
abu bakar <abubakar850772@gmail.com>
2025-01-28 23:56:17 +0600

    feat: integrate socket.io

--- websocket/deep_dive_into_socket.io/socket-chat-example/index.js
+++ websocket/deep_dive_into_socket.io/socket-chat-example/index.js
@@ -2,11 +2,14 @@ import express from 'express'
 import {createServer} from 'http'
 import {fileURLToPath} from 'url'
 import {dirname,join} from 'path'
+import {Server} from 'socket.io'
 
 const app = express()
 const port = 9191;
 
 const httpServer =  createServer(app);
+
+const io = new Server(httpServer)
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -14,6 +17,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.get("/", (req, res) => {
     res.sendFile(join(__dirname, "index.html"))
+})
+
+io.on("connection", (socket) => {
+    console.log("a user is connected")
+    socket.on("disconnect", () => {
+        console.log("a user is disconnected")
+    })
 })
 
 httpServer.listen(port, () => {
