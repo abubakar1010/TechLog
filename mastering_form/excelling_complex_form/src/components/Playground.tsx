bfede582554591a7995aea4096a5315fd66c0ef3
abu bakar <abubakar850772@gmail.com>
2025-03-18 15:19:19 +0600

    feat: implement generic form

--- mastering_form/excelling_complex_form/src/components/Playground.tsx
+++ mastering_form/excelling_complex_form/src/components/Playground.tsx
@@ -1,7 +1,10 @@
 import { z } from "zod";
-import { GenericForm } from "./form/GenericForm";
+import { GenericForm, TGenericFormRef } from "./form/GenericForm";
 import { Button } from "./ui/button";
 import { TextFields } from "./form/TextFields";
+import { useRef, useState } from "react";
+import { FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Textarea } from "./ui/textarea";
 
 const ZFormSchema = z.object({
 	name: z
@@ -10,6 +13,9 @@ const ZFormSchema = z.object({
 		.max(50, "Name must be at most 50 characters"),
 	email: z.string().email(),
 	age: z.coerce.number().min(19, { message: "Age must be at least 19" }),
+	content: z
+		.string()
+		.min(5, { message: "Content must be at least 5 characters" }),
 });
 
 type TForm = z.infer<typeof ZFormSchema>;
@@ -18,9 +24,17 @@ const initialValues: TForm = {
 	name: "",
 	email: "",
 	age: 0,
+	content: "",
 };
 
 export const Playground = () => {
+	const formRef = useRef<TGenericFormRef<TForm>>(null);
+	const [values, setValues] = useState<TForm | null | undefined>(null)
+
+	const handleReset = (value: Partial<TForm>) => {
+		formRef.current?.reset(value);
+	};
+
 	return (
 		<div>
 			<h1>This is the Playground page</h1>
@@ -28,19 +42,60 @@ export const Playground = () => {
 				initialValues={initialValues}
 				schema={ZFormSchema}
 				onSubmit={(values) => {
-					alert("form submitted");
+					alert(JSON.stringify(values));
 					console.log(values);
 				}}
+				ref={formRef}
 			>
 				<div className=" space-y-7 my-8">
 					<TextFields<TForm> name="name" label="Name" />
 					<TextFields<TForm> name="email" label="Email" type="email" />
 					<TextFields<TForm> name="age" label="Age" type="number" />
-					<Button type="submit" className="cursor-pointer">
-						Submit
-					</Button>
+					<div>
+						<FormField
+							control={formRef.current?.control}
+							name="content"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Content</FormLabel>
+									<Textarea {...field} placeholder="Enter your content" />
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<div className=" flex justify-end gap-6">
+						<Button
+							onClick={() =>
+								handleReset({
+									name: "rayhan",
+									content: "Hey dude! How are you doing",
+									age: 30,
+									email: "rayhan@example.com",
+								})
+							}
+							className="cursor-pointer"
+						>
+							Reset
+						</Button>
+						<Button type="submit" className="cursor-pointer">
+							Submit
+						</Button>
+						<Button type="button" onClick={() => {
+							console.log(formRef.current?.getValues())
+							setValues(formRef.current?.getValues())
+						}}>
+                            Show Values
+                        </Button>
+					</div>
 				</div>
 			</GenericForm>
+
+			<div>
+				{
+					values && JSON.stringify(values, null, 2)
+				}
+			</div>
 		</div>
 	);
 };
--- mastering_form/excelling_complex_form/src/components/form/GenericForm.tsx
+++ mastering_form/excelling_complex_form/src/components/form/GenericForm.tsx
@@ -1,9 +1,30 @@
-import { ReactNode } from "react";
-import { DefaultValues, SubmitHandler, useForm } from "react-hook-form";
+import { ReactNode, Ref, useImperativeHandle } from "react";
+import {
+	Control,
+	DefaultValues,
+	FieldValues,
+	FormState,
+	Path,
+	SubmitHandler,
+	useForm,
+	UseFormReturn,
+} from "react-hook-form";
 import { Form } from "../ui/form";
 import { GenericFormContext } from "./CustomContext";
 import { z, ZodType } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+
+export type TGenericFormRef<TFormFieldValues extends FieldValues> = {
+	control: Control<TFormFieldValues>;
+	form: UseFormReturn<TFormFieldValues>;
+	formState: FormState<TFormFieldValues>;
+	getValues: () => TFormFieldValues;
+	setValues: (
+		name: Path<TFormFieldValues>,
+		value: TFormFieldValues[Path<TFormFieldValues>]
+	) => void;
+	reset: (value?: Partial<TFormFieldValues> | undefined) => void;
+};
 
 type TGenericFormProps<TSchema extends ZodType> = {
 	children: ReactNode;
@@ -11,12 +32,21 @@ type TGenericFormProps<TSchema extends ZodType> = {
 	initialValues: Partial<z.infer<TSchema>>;
 	onSubmit: SubmitHandler<z.infer<TSchema>>;
 	mode?: "onChange" | "onSubmit" | "onBlur";
+	ref?: Ref<TGenericFormRef<z.infer<TSchema>>>;
 };
 
+export const GenericForm = <TSchema extends ZodType>(
+	props: TGenericFormProps<TSchema>
+) => {
+	const {
+		children,
+		schema,
+		initialValues,
+		onSubmit,
+		mode = "onChange",
+		ref,
+	} = props;
 
-export const GenericForm = <TSchema extends ZodType>(props: TGenericFormProps<TSchema>) => {
-	const {children, schema, initialValues, onSubmit, mode="onChange"} = props;
-	
 	type TFormValues = z.infer<TSchema>;
 
 	const form = useForm<TFormValues>({
@@ -25,6 +55,21 @@ export const GenericForm = <TSchema extends ZodType>(props: TGenericFormProps<TS
 		defaultValues: initialValues as DefaultValues<TFormValues>,
 	});
 
+	useImperativeHandle(ref, () => ({
+		control: form.control,
+		form: form,
+		formState: form.formState,
+		getValues: form.getValues,
+		setValues: (
+			name: Path<TFormValues>,
+			value: FieldValues[Path<TFormValues>]
+		) => {
+			form.setValue(name, value);
+		},
+		reset: (values?: Partial<TFormValues> | undefined) => {
+			form.reset(values as TFormValues);
+		},
+	}));
 
 	return (
 		<GenericFormContext value={{ control: form.control }}>
