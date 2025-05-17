65f51046d98f3c8229653cf0f41424e8c32d7773
abu bakar <abubakar850772@gmail.com>
2025-05-17 20:12:56 +0600

    feat(patterns): add mirror number pyramid pattern implementation in C

---
+++ dsa-problem-solving/problems/loop/pattern/number-patterns/number-pyramid-mirror.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int height;
+    printf("Enter the height of pyramid: ");
+    scanf("%i", &height);
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = i; j <= height - 1; j++)
+        {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%i", j);
+        }
+        for (int j = 1; j <= i-1; j++)
+        {
+            printf("%i", j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
