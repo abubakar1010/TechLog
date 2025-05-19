3bd836e69944a0d90a4122a1ffa1e0c004ddeac0
abu bakar <abubakar850772@gmail.com>
2025-05-19 20:18:02 +0600

    feat(primes-in-range): add C++ implementation to find prime numbers in a given range

---
+++ dsa-problem-solving/problems/loop/digit-problems/primes-in-range/primes-in-range.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    int starting_point, ending_point;
+
+    cout << "Enter The Starting Point: ";
+
+    cin >> starting_point;
+
+    cout << "Enter The Ending Point: ";
+
+    cin >> ending_point;
+
+    for (int i = starting_point; i <= ending_point; i++)
+    {
+        bool isPrime = true;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime && i > 1)
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
