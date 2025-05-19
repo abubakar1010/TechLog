6eac98b8fd2cbce86958eb70aa965bf64ba43ae5
abu bakar <abubakar850772@gmail.com>
2025-05-19 20:15:59 +0600

    feat(sqrt-and-squares): add C++ implementations for square root and square of first N digits

---
+++ dsa-problem-solving/problems/loop/digit-problems/squares-of-first-n-digit/squares-of-first-n-digit.cpp
@@ -0,0 +1,21 @@
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    int n;
+
+    cout<<"Enter the value of N: ";
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout<<pow(i, 2)<<" ";
+    }
+    
+
+    
+
+    return 0;
+}
