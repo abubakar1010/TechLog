ec8e564a7b2d243e293bf03c4630a1338f2b0c8b
abu bakar <abubakar850772@gmail.com>
2025-05-22 20:05:36 +0600

    feat(InputGroup): add InputGroup component for structured input handling with error display

---
+++ mastering-form/custom-form-library/src/components/InputGroup.jsx
@@ -0,0 +1,39 @@
+import { cn } from "../helper/cn";
+
+export const InputGroup = ({
+	type,
+	name,
+	id,
+	placeholder,
+	label,
+	value,
+	error,
+	onChange,
+	onBlur,
+	onFocus,
+}) => {
+	return (
+		<div className=" bg-white p-4 rounded-md shadow-xl text-black flex flex-col gap-2 w-3xl mx-auto">
+			<label className=" text-xl  text-gray-700" htmlFor={id}>
+				{label}
+			</label>
+			<input
+				className={cn(
+					"py-3 px-2 text-xl outline-none border-gray-500 rounded-md border",
+					{
+						"border-red-600": error,
+					}
+				)}
+				type={type}
+				name={name}
+				id={id}
+				value={value}
+				placeholder={placeholder}
+				onChange={onChange}
+				onFocus={onFocus}
+				onBlur={onBlur}
+			/>
+			{error && <p className=" text-2xl font-medium text-red-500">{error}</p>}
+		</div>
+	);
+};
