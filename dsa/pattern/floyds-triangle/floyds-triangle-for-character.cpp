10cf0b77567d96ef17cf8e3d9d4b9b480e858597
abubakar1010 <abubakar850772@gmail.com>
2026-02-19 06:06:57 +0600

    feat(floyd's triangle): print floyd's triangle pattern for number and character

---
+++ dsa/pattern/floyds-triangle/floyds-triangle-for-character.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    int n;
+
+    cin >> n;
+
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int v = s;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << (char)(v + 65) ;
+            v++;
+            if(j == i){
+                s = v;
+            }
+        }
+        cout << endl;
+    }
+    
+
+        return 0;
+}
