5b60d658a59568358a944c98ac26972e7ea61181
abubakar1010 <abubakar850772@gmail.com>
2026-02-22 03:59:23 +0600

    feat(hollow-diamond):print hollow pyramid pattern

---
+++ dsa/pattern/hollow-diamond-pattern/hollow-diamond-pattern.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    int n = 0;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+
+        for (int j = i; j > 0; j--)
+        {
+            if (j == 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (j == 0)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+
+        for (int j = n - i - 2; j > 0; j--)
+        {
+            if (j == 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
