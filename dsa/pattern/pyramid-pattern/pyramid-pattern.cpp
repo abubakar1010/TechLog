9fd4dc7ad59d3194497b1f05450367b3c3e6d374
abubakar1010 <abubakar850772@gmail.com>
2026-02-22 03:57:54 +0600

    feat(pyramid):print pyramid pattern

---
+++ dsa/pattern/pyramid-pattern/pyramid-pattern.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    int n = 0;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j + 1;
+        }
+
+        for (int j = i; j > 0; j--)
+        {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
