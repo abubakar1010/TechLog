0be7feb0339eebbe814fca32d4e216c29d227613
abubakar1010 <abubakar850772@gmail.com>
2026-02-22 03:58:11 +0600

    feat(hollow-pyramid):print hollow pyramid pattern

---
+++ dsa/pattern/hollow-pyramid/hollow-pyramid.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    int n = 0;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+
+        for (int j = i; j > 0; j--)
+        {
+            if (j == 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+
+    return 0;
+}
