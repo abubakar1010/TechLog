936c82a64f3a584cac96c0ec29623aaa2b5222d9
abubakar1010 <abubakar850772@gmail.com>
2026-02-22 03:59:16 +0600

    feat(diamond):print  pyramid pattern

---
+++ dsa/pattern/diamond-pattern/diamond-pattern.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    int n = 0;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << j + 1;
+        }
+
+        for (int j = i; j > 0; j--)
+        {
+            cout << j;
+        }
+
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << j + 1;
+        }
+
+        for (int j = n - i - 2; j > 0; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
